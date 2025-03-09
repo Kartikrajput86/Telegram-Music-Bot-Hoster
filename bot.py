8f39db90b7b7129680a65556d0933d9a7e53bdee
Kartikrajput86 <sakshisingh93540@gmail.com>
2025-03-09 23:02:48 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,14 @@
+import os
+from pyrogram import Client, filters
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Client("MusicBot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+@bot.on_message(filters.command("start"))
+def start(client, message):
+    message.reply_text("Hello! I'm a Music Bot. Send /play to stream music.")
+
+bot.run()
